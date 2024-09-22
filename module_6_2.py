bf4db94bca580d54cb2c2862bb79a79ce443f156
IngaKon <kononova.inga@list.ru>
2024-09-22 15:36:10 +0300

    Update module_6_2.py 1

--- module_6_2.py
+++ module_6_2.py
@@ -1,41 +1,51 @@
 class Vehicle:
-    __COLOR_VARIANTS = ['green','blue','white', 'red', 'black']
-    def __init__(self, owner, __model,__color, __engine_power):
+    def __init__(self, owner, __model, __color, __engine_power):
         self.owner = owner
         self.__model = __model
-        self.__color = __color
         self.__engine_power = __engine_power
+        self.__color = __color
+
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
     def get_model(self):
-        print(f'Модель: {self.__model}')
+        return (f' Модель:{self.__model}')
 
     def get_horsepower(self):
-        return (f'Мощность: {self.__engine_power}')
+        return (f' Мощность двигателя: {self.__engine_power}')
 
     def get_color(self):
-        return (f'Цвет: {self.__color}')
+        return (f' Цвет транспорта: {self.__color}')
 
     def print_info(self):
-        Vehicle.get_model(self)
-        Vehicle.get_horsepower(self)
-        Vehicle.get_color(self)
-        print(f'Владелец: {self.owner}')
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f' Владелец: {self.owner}')
+
 
     def set_color(self, new_color):
-        if new_color.lower() in self.__COLOR_VARIANTS:
+        new_color = new_color.casefold()
+        self.new_color = new_color
+        if new_color in Vehicle.__COLOR_VARIANTS:
             self.__color = new_color
         else:
-            print(f'Нельзя сменить цвет на {new_color}')
+            print(f'Нельзя сменить цвет на {self.new_color}')
 
 class Sedan(Vehicle):
-    __PASSENGER_LIMIT = 5
+    def __init__(self, owner, model, color, engine_power):
+        super().__init__(owner, model, color, engine_power)
+        __PASSENGERS_LIMIT = 5
 
-    def __init__(self, owner, model, engine_power, color):
-        super().__init__(owner, model, engine_power, color)
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle1 = Sedan('Надежда', 'Jaecoo J7', 'green', 186)
 
-vehicle1 = Sedan('Fedos', 'Toyota Mark II',  500,'blue' )
+# Изначальные свойства
 vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
 vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
 vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
 vehicle1.print_info()
